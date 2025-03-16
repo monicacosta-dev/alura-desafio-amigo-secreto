0b031bd38c52190d0551b6c5c5218a8908ee15fb
Mônica Costa <51413784+monicacosta-dev@users.noreply.github.com>
2025-03-16 18:12:20 -0300

    Arquivos de Projetos
    Arquivos do projeto incluido.

---
+++ app.js
@@ -0,0 +1,45 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let names = []
+
+
+function limparCampo(){
+    let nome = document.querySelector('input')
+    nome.value = ""
+}
+
+
+function adicionarAmigo(){
+    
+    let nome = document.querySelector('input').value
+    if(nome.trim() !== ""){
+        names.push(nome)
+        
+        let listaAmigos = document.getElementById('listaAmigos')
+        let lista = criarLista('li', nome)
+        listaAmigos.appendChild(lista)
+
+
+        limparCampo()
+    }
+}
+
+function sortearAmigo(){
+    if(names.length > 0){
+        let indiceAleatorio = Math.floor(Math.random() * names.length)
+        let nomeAleatorio = names[indiceAleatorio]
+        
+        
+        let resultado = document.getElementById('resultado')
+        let listaSorteadaDeNomes = criarLista('li', nomeAleatorio)
+        resultado.appendChild(listaSorteadaDeNomes)
+
+        names.splice(indiceAleatorio, 1)
+    }
+
+}
+
+function criarLista(tag, nome){
+    let lista = document.createElement(tag)
+    lista.textContent = nome
+    return lista
+}
